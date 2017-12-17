04cfd6ea6fc36f4d929c685300aac18631466385
Greg Brewton <gbrewton1980@gmail.com>
2017-12-17 16:41:02 -0600

    Gets routing functions up and running

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const appRoutes: Routes = [
+  { path: 'test', component: AppComponent},
+  //{ path: 'login', component: LoginComponent}, //login page
+  //{ path: 'notes', component: NotesComponent}, //landing page / note list
+  //{ path: 'note/:id', component: NoteComponent}, //expanded single note view
+  //{ path: '', redirectTo: './notes', pathMatch: 'full'}, //take user to notes if URL route is empty
+  //{ path: '**', component: PageNotFoundComponent }//catch-all for URL errors
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true } // debugging purposes
+    ),
     BrowserModule
   ],
   providers: [],
