9098b2e5edc5a1a1c5938f3da5555252f665eed7
Greg Brewton <gbrewton1980@gmail.com>
2017-12-17 16:19:28 -0600

    Initial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+    {
+      path: '',
+      redirectTo: 'login',
+      pathMatch: 'full'
+    },
+    {
+      path: '',
+      component: AppComponent
+    }
+  ];
