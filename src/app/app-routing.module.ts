d2ac19938c9efffa6c2535a2ada2757431144a19
Greg Brewton <gbrewton1980@gmail.com>
2017-12-31 17:12:45 -0600

    Added note search component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { NoteDetailComponent }  from './note-detail/note-detail.component';
 import { NoteAddComponent } from './note-add/note-add.component';
+import { NoteSearchComponent } from './note-search/note-search.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: NoteDetailComponent },
-  { path: 'addNote', component: NoteAddComponent }
+  { path: 'addNote', component: NoteAddComponent },
+  { path: 'noteSearch', component: NoteSearchComponent }
 ];
 
 @NgModule({
