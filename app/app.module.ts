874bf735dbd70b3e64b2cab4d601288aeadd9df7
Greg Brewton <gbrewton1980@gmail.com>
2018-01-01 20:33:19 -0600

    adding files made before fatal crash
    could not push changes, trying to save the files I changed

---
+++ app/app.module.ts
@@ -0,0 +1,80 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+//fake backend for testing notes
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+//fake backend for testing login/registration
+import { fakeBackendProvider } from './fake-backend';
+
+
+//routing
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NoteDetailComponent } from './note-detail/note-detail.component';
+import { NoteSearchComponent } from './note-search/note-search.component';
+import { MessagesComponent } from './messages/messages.component';
+import { NoteAddComponent } from './note-add/note-add.component';
+import { AlertComponent } from './alert/alert.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component'
+
+//services and helpers
+import { NoteService } from './note.service';
+import { MessageService } from './message.service';
+import { AuthGuard } from './auth-guard';
+import { JwtInterceptor } from './jwt-interceptor';
+import { AlertService } from './alert.service';
+import { UserService } from './user.service';
+import { AuthenticationService } from './authentication.service';
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    NoteDetailComponent,
+    MessagesComponent,
+    NoteSearchComponent,
+    NoteAddComponent,
+    AlertComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  providers: [
+    NoteService,
+    MessageService,
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/note.service.ts
@@ -0,0 +1,121 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Note } from './note';
+import { MessageService } from './message.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
+export class NoteService {
+
+  private notesUrl = 'api/notes';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /** GET notes from the server */
+  getNotes (): Observable<Note[]> {
+    return this.http.get<Note[]>(this.notesUrl)
+    .pipe(
+      catchError(this.handleError('getNotes', []))
+    );
+  }
+
+  /** GET note by id. Return `undefined` when id not found */
+  getNoteNo404<Data>(id: number): Observable<Note> {
+    const url = `${this.notesUrl}/?id=${id}`;
+    return this.http.get<Note[]>(url)
+      .pipe(
+        map(notes => notes[0]), // returns a {0|1} element array
+        tap(h => {
+          const outcome = h ? `fetched` : `did not find`;
+          this.log(`${outcome} note id=${id}`);
+        }),
+        catchError(this.handleError<Note>(`getNote id=${id}`))
+      );
+  }
+
+  /** GET note by id. Will 404 if id not found */
+  getNote(id: number): Observable<Note> {
+    const url = `${this.notesUrl}/${id}`;
+    return this.http.get<Note>(url).pipe(
+      tap(_ => this.log(`fetched note id=${id}`)),
+      catchError(this.handleError<Note>(`getNote id=${id}`))
+    );
+  }
+
+  /* GET notes whose name contains search term */
+  searchNotes(term: string): Observable<Note[]> {
+    if (!term.trim()) {
+      // if not search term, return empty note array.
+      return of([]);
+    }
+    return this.http.get<Note[]>(`api/notes/?name=${term}`).pipe(
+      tap(_ => this.log(`found notes matching "${term}"`)),
+      catchError(this.handleError<Note[]>('searchNotes', []))
+    );
+  }
+
+  //////// Save methods //////////
+
+  /** POST: add a new note to the server */
+  addNote (note: Note): Observable<Note> {
+    console.log("%c addnote() called", "color: blue");
+    return this.http.post<Note>(this.notesUrl, note, httpOptions).pipe(
+      tap((note: Note) => this.log(`added note w/ name=${note.name} and content=${note.content}`)),
+      catchError(this.handleError<Note>('addNote'))
+    );
+  }
+
+  /** DELETE: delete the note from the server */
+  deleteNote (note: Note | number): Observable<Note> {
+    const id = typeof note === 'number' ? note : note.id;
+    const url = `${this.notesUrl}/${id}`;
+
+    return this.http.delete<Note>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted note id=${id}`)),
+      catchError(this.handleError<Note>('deleteNote'))
+    );
+  }
+
+  /** PUT: update the note on the server */
+  updateNote (note: Note): Observable<any> {
+    return this.http.put(this.notesUrl, note, httpOptions).pipe(
+      tap(_ => this.log(`updated note id=${note.id}`)),
+      catchError(this.handleError<any>('updateNote'))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a NoteService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('NoteService: ' + message);
+  }
+}
