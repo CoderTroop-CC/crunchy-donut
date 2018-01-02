90e462a33aaad29b3f538c1c95c653bdce029dab
David Woodard <mrdavidwoodard@gmail.com>
2018-01-01 18:30:05 -0800

    updated the server to use the correct port

--- server/models/user.js
+++ server/models/user.js
@@ -18,6 +18,11 @@ var userSchema = new Schema({
         required: true, 
         unique: true 
     },
+    email: {
+        type: String, 
+        required: true, 
+        unique: true 
+    },
     password: { 
         type: String, 
         required: true 
